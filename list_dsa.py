bb25c781aaae7a620248b00e040b1166df4568ae
khushi <khusbumandal2060@gmail.com>
2024-12-22 11:45:42 +0545

    How to implement pyton in DSA

---
+++ list_dsa.py
@@ -0,0 +1,14 @@
+#LIST:
+Mountain=['Sagarmatha','Dhaulagiri','Makalu']
+print(type(Mountain))
+print(len(Mountain))
+print(Mountain)
+
+Mountain.append("Kilimanjaaro")
+print(Mountain)
+
+Mountain.pop()
+print(Mountain)
+
+Mountain.pop(2)
+print(Mountain)
