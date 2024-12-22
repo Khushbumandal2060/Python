6213389bbe29842196eb1ca4e156a18b45541ce4
khushi <khusbumandal2060@gmail.com>
2024-12-22 11:44:29 +0545

    if_else statement

---
+++ if_else.py
@@ -0,0 +1,10 @@
+#getting data from the user
+pin=1234
+data1=int(input("Enter yout pin number:"))
+print("data1:",data1)
+if data1==pin:
+    print('Wlcome to NMB Bank !!!')
+else:
+    print('Please enter your correct PIN number !')
+
+    
