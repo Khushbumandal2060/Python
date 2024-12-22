fd797cfc8461f59919153ee16f74ff4e7a142ae5
khushi <khusbumandal2060@gmail.com>
2024-12-22 11:48:49 +0545

    How to print odd or even number

---
+++ odd_even.py
@@ -0,0 +1,11 @@
+#TASK1: check the entered number is odd or even
+
+num=int(input("Enter the number:"))
+print("num:",num)
+if num%2==0:
+          print('Entered number is even !!!')
+else:
+          print('entered number is odd !')
+
+
+   
