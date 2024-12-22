80ea6479ced9f9e85fe2a86650fd2a487765c954
khushi <khusbumandal2060@gmail.com>
2024-12-22 11:55:32 +0545

    Creation of Variable

---
+++ variable.py
@@ -0,0 +1,5 @@
+#CODE2:creation of variable
+
+Student1 = 'Ram' #Ram is a data, which is being stored in a  variable named student1
+print(Student1) #print is used to display the data stored in a variable named student1....
+
