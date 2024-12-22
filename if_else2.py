71ec9cb2dffce2f4e2cdbcc3df409693d66c6a43
khushi <khusbumandal2060@gmail.com>
2024-12-22 13:46:42 +0545

    if_else statement

---
+++ if_else2.py
@@ -0,0 +1,10 @@
+age = 22
+
+if age >= 19:
+    print("You are an adult")
+    print("You can vote")
+elif age < 19 and age > 3:
+    print("You are in school")
+
+else:
+    print("You are a child")
